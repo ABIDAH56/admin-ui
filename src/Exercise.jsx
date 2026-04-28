296c9e8df712ea540665547a7f0e36ef562092b1
ABIDAH56 <abidahsyujana@gmail.com>
2026-04-28 15:31:19 +0700

    UTS_15333

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,36 +1,21 @@
 // src/Exercise.jsx
-import React, { useEffect, useState } from 'react';
-import UserCard from './UserCard';
-import { getUsers } from './Services';
+import PostCard from "./PostCard";
+import postsData from "./data/postsData";
 
 function Exercise() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getUsers();
-        setUsers(data);
-      } catch (error) {
-        console.error("[Component] Gagal menampilkan data:", error.message);
-      }
-    };
-    
-    fetchData();
-  }, []); // Dependency array kosong agar hanya berjalan 1x saat render pertama
-
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-        User Cards
-      </h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-        {users.map((user, index) => (
-          // Menggunakan index sebagai key (sementara) dan Spread Operator
-          <UserCard key={index} {...user} />
-        ))}
-      </div>
-    </div>
+    <>
+      <div className="min-h-screen bg-gray-06 p-10 pl-20 pr-20">
+        <h1 className="text-3xl font-bold text-center mb-10 text-special-red2">
+          Post Cards
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 w-full mx-auto px-4">
+          {postsData.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
+        </div>
+      </div>    
+    </>
   );
 }
 
